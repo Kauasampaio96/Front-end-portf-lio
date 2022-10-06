f113859613961a52f692ea599e5fa333b819e641
Kauã <kauasampaio33@hotmail.com>
2022-10-06 15:03:27 -0300

    primeito commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const sidenav = document.getElementById('sidenav')
+
+window.onscroll = function(){
+    scrollBtn()
+}
+
+function abrirMenu(){
+    console.log('rodou')
+    sidenav.style.right = '0px';
+}
+
+
+function fecharMenu(){
+    sidenav.style.right='-520px';
+}
+
+
+function scrollBtn(){
+    var btn = document.getElementById('btntopo')
+    if(document.documentElement.scrollTop > 600){
+        btn.style.display ='block';
+    }
+    else{
+        btn.style.display ='none';
+    }
+}
